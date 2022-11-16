032b59295f6f9a22e3f1e5ed234229f07f1435cc
SaveliyLapin <saveliylapin@gmail.com>
2022-11-17 06:59:06 +0700

    Added Car,CarController.java,cars.html

---
+++ src/main/java/web/model/Car.java
@@ -0,0 +1,32 @@
+package web.model;
+
+public class Car {
+
+    private Long id;
+    private String model;
+    private int series;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
